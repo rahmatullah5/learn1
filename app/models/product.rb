ed3667e2c866c6af66f04ac0bb5c024093a1dad5
Sam Ruby <rubys@intertwingly.net>
2017-06-12 17:06:10 +0700

    Add Cart & Some Redesign at index.erb

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,13 @@ class Product < ApplicationRecord
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
-
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item #...
+  private
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+    throw :abort end
+  end
 end
