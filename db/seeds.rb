c37f65c516b741e25a0179ea5029f32792a716e2
Muhammad Rahmatullah <rahmat@41studio.com>
2017-06-12 11:50:13 +0700

    New Product With Design implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create!(
+  title: 'Books of the dead',
+  description:
+  %{<p>
+    <em>Native Apps, Multiple Platforms</em>
+    Answer me dude
+    </p>},
+  image_url: '7apps.jpg',
+  price: 25.00
+)
